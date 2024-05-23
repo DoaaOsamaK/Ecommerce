3d5b5bea5fde583fdd9fb853a9bb02ed9f3bda5b
Augalassia <doaakhalaf1outlook@gmail.com>
2024-05-23 18:41:23 +0300

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,46 @@
-import Header1 from "./components/header/Header1.jsx";
 import Header2 from "./components/header/Header2.jsx";
+import Header1 from "./components/header/Header1.jsx";
 import Header3 from "./components/header/Header3.jsx";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./Theme.jsx";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme.js";
+import Hero from "./components/hero/Hero.jsx";
+import Main from "./components/mainContent/main.jsx";
+import Footer from "./components/footer/footer.jsx";
+import ScrollToTop from "./components/scroll/ScrollToTop.jsx";
 
 function App() {
   const [theme, colorMode] = useMode();
   return (
-    <>
-      <ColorModeContext.Provider 
-// @ts-ignore
-      value={colorMode}>
-        <ThemeProvider 
-// @ts-ignore
-        theme={theme}>
-          <CssBaseline />
-          <Header1 />
-          <Header2 />
-          <Header3 />
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    </>
+    <ColorModeContext.Provider
+      // @ts-ignore
+      value={colorMode}
+    >
+      <ThemeProvider
+        // @ts-ignore
+        theme={theme}
+      >
+        <CssBaseline />
+
+        <Header1 />
+        <Header2 />
+        <Header3 />
+
+        <Box
+          bgcolor={
+            // @ts-ignore
+            theme.palette.bg.main
+          }
+        >
+          <Hero />
+          <Main />
+        </Box>
+
+        <Footer />
+
+
+        <ScrollToTop />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+const Hero = () => {
+  return (
+    <Swiper
+      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      spaceBetween={50}
+      slidesPerView={1}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Hero;
